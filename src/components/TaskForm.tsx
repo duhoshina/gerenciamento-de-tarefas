c1f6f7abdae4130798c3603fc0d931679040deb4
Luis Eduardo Hoshina <duhoshina@gmail.com>
2022-08-28 14:22:54 -0300

    :sparkles: feat: Add Funções do Forms

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, {useState, ChangeEvent, FormEvent} from 'react'
 
+// css
 import styles from './TaskForm.module.css'
+
+// interfaces
+import {ITask} from '../interfaces/Task'
 
 interface Props {
   btnText: string
 }
 
 const TaskForm = ({ btnText }: Props) => {
+
+  // states que batem com o ITask
+  const [id, SetId] = useState<number>(0)
+  const [title, SetTitle] = useState<string>("")
+  const [difficulty, SetDifficulty] = useState<number>(0)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    switch(event.target.name) {
+      case 'title':
+        SetTitle(event.target.value)
+      break
+      case 'difficulty':
+        SetDifficulty(parseInt(event.target.value))
+      break
+    }
+
+  }
+
+  const addTaskHandler = () => {
+
+  } 
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={addTaskHandler} >
       <div className={styles.input_container}>
         <label htmlFor="title">Título</label>
-        <input type="text" name="title" placeholder="Ex: Terminar de ver as videos aulas de TS" />
+        <input type="text" name="title"  onChange={handleChange} placeholder="Ex: Terminar de ver as videos aulas de TS" />
       </div>
       <div className={styles.input_container}>
-        <label htmlFor="dificuldade">Dificuldade</label>
-        <input type="text" name="dificuldade" placeholder="Dificuldade da Tarefa" />
+        <label htmlFor="difficulty">Dificuldade</label>
+        <input type="text" name="difficulty" onChange={handleChange} placeholder="Dificuldade da Tarefa" />
       </div>
       <input type="submit" value={btnText} />
     </form>
