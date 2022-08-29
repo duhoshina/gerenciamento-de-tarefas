d5dc28cb8904028ea74d8728a4e6976472e3bcb3
Luis Eduardo Hoshina <duhoshina@gmail.com>
2022-08-29 11:36:55 -0300

    :sparkles: feat: Click Close Modal

--- src/App.tsx
+++ src/App.tsx
@@ -22,16 +22,22 @@ function App() {
     SetTaskList(taskList.filter((item) => item.id !== id))
   }, [taskList])
 
+  const closeModal = () => {
+    if(showModal){
+      SetShowModal(false)
+    } 
+  }
+
   return (
     <div>
       <Header />
-      <main className={styles.main}>
+      <main onClick={closeModal} className={styles.main}>
         <h2 className={styles.h2}>O que deve ser feito hoje?</h2>
         <TaskForm  SetTaskList={SetTaskList} taskList={taskList} btnText='Criar Tarefa' />
         <h2 className={styles.h2}>Suas Tarefas</h2>
         <TaskList deleteTask={deleteTask}  SetShowModal={SetShowModal} taskList={taskList} />
-        {showModal && <Modal SetShowModal={SetShowModal} taskList={taskList} />}
       </main>
+      {showModal && <Modal SetShowModal={SetShowModal} taskList={taskList} />}
       <Footer />
     </div>
   );
